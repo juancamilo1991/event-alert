4fa8969044dfc0b062a18b66f3b59c5e2b0f1dce
Camilo Galeano <juan_camilo50@hotmail.com>
2023-03-22 17:53:12 +0100

    only one getPosts function for all and filtered posts

--- event-alert-frontend/src/api/blogPosts.ts
+++ event-alert-frontend/src/api/blogPosts.ts
@@ -1,34 +1,35 @@
 import axios, { AxiosResponse } from 'axios';
-import { ChannelPost } from '../types/types';
+import { ChannelPost, RequestError } from '../types/types';
 import { client } from './config/axios';
 
 
-const ALLPOSTS_ENDPOINT = 'blog/channel-posts';
-const FILTERED_POSTS_ENDPOINT = 'blog/posts';
+export const ALLPOSTS_ENDPOINT = 'blog/channel-posts';
+export const FILTERED_POSTS_ENDPOINT = 'blog/posts';
 
-export async function getAllPosts(area_code: number): Promise<ChannelPost[]> {
-   const result = getPosts(ALLPOSTS_ENDPOINT, area_code);
-   return (await result).data
-   /* try {
-      const result = await client.get(ALLPOSTS_ENDPOINT, { params: { area: area_code } });
-      return result.data;
-   } catch (error) {
-      return error as Promise<ChannelPost[]>;
-   } */
-};
-
-export async function getFilteredPosts() {
-
-}
-
-async function getPosts(endpoint: string, area_code: number, category?: string, order?: string ): Promise<AxiosResponse<any, any>> {
+export async function getPosts(endpoint: string, area_code: number, category?: string, order?: string): Promise<ChannelPost[] | RequestError> {
    try {
-      return await client.get(endpoint, { params: { area: area_code,  category: category, order: order} });
+      return await client.get(endpoint, {
+         params: {
+            area: area_code,
+            category: category,
+            order: order
+         }
+      });
    } catch (error) {
-      return error as Promise<AxiosResponse<any, any>>;
+      return new Promise<RequestError>((resolve, reject) => {
+         if (axios.isAxiosError(error)) {
+            const axiosError: RequestError = {
+               status: error.status as number,
+               message: error.message
+            }
+            resolve(axiosError)  
+         }
+         else {
+            resolve(error as RequestError);
+         }
+      })
    }
 }
-
 
 
 
