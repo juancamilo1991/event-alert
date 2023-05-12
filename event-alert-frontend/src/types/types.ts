9e75abc9a271574165591ac4c66d6d42985e5d22
Camilo Galeano <galeajua@students.zhaw.ch>
2023-05-12 15:10:46 +0200

    added new types for success message and WritePost component

--- event-alert-frontend/src/types/types.ts
+++ event-alert-frontend/src/types/types.ts
@@ -9,7 +9,14 @@ export interface ChannelPost {
     likesCount: number,
     publicationDate: string,
     user: Author,
+    area: number
 }
+
+export type AuthHeaders = {
+    'Content-Type': string,
+    'Authorization': string
+}
+
 
 export interface LoginData {
     username: string,
@@ -22,12 +29,35 @@ interface EmailData {
 
 export interface RequestError {
     myStatus: number | undefined,
-    message: number | undefined
+    message: string | undefined
 }
+
+export interface SuccessMessage {
+    message: string | undefined;
+}
+export interface SearchAreaProps {
+    displayError: (error: RequestError) => void,
+    displayPosts: (posts: ChannelPost[]) => void,
+}
+
+export interface WriteProps {
+    isLoggedIn: string;
+    area: string;
+    category: string;
+    isDeleteInput: boolean;
+  }
+export interface SideNavProps {
+    isActive: boolean,
+    closeSideNav: () => void,
+}
+
+export type RegistrationData = LoginData & EmailData;
+export type SideBarProps = SideNavProps & SearchAreaProps;
+export type WritePostProps = WriteProps & SearchAreaProps;
+export type SearchProps = SearchAreaProps & SideNavProps & WriteProps;
 
 export function isRequestError(object: unknown): object is RequestError {
     return Object.prototype.hasOwnProperty.call(object, "myStatus")
         && Object.prototype.hasOwnProperty.call(object, "message");
 }
 
-export type RegistrationData = LoginData & EmailData;
