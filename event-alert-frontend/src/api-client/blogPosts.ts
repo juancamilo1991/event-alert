b6826c7305d33123ef9ce368680da127379ac462
Camilo Galeano <galeajua@students.zhaw.ch>
2023-05-12 15:21:22 +0200

    new reusable function for api calls

--- event-alert-frontend/src/api-client/blogPosts.ts
+++ event-alert-frontend/src/api-client/blogPosts.ts
@@ -1,32 +1,42 @@
 import axios, { AxiosResponse } from 'axios';
-import { ChannelPost, RequestError } from '../types/types';
+import { ChannelPost, AuthHeaders, RequestError } from '../types/types';
 import { client } from './config/axios';
 
 
 export const ALLPOSTS_ENDPOINT = 'blog/channel-posts';
 export const FILTERED_POSTS_ENDPOINT = 'blog/posts';
+export const POST_BLOG_ENDPOINT = 'blog/create-post';
 
-export async function getPosts(endpoint: string, area_code: number, category?: string, order?: string): Promise<ChannelPost[] | RequestError> {
+
+export async function makeApiRequest(endpoint: string, area_code: number, method?: string, category?: string, order?: string, title?: string, text?: string, headers?: AuthHeaders): Promise<ChannelPost[] | RequestError> {
    try {
-      const result = await client.get(endpoint, {
+      if (method === 'POST') {
+         const postResult = await client.post(endpoint, {
+            title: title,
+            text: text,
+            category: category,
+            area: area_code
+         }, {
+            headers: headers
+         });
+         return postResult.data;
+      }
+      const getResult = await client.get(endpoint, {
          params: {
             area: area_code,
             category: category,
             order: order
          }
       });
-      console.log(result.data);
-      return result.data;
+      return getResult.data;
    } catch (error) {
          if (axios.isAxiosError(error)) {
-            console.log(error);
             const axiosError: RequestError = {
                myStatus: error.response?.status,
                message: error.response?.data
             }
             return axiosError;
          }
-         console.log(error);
         return error as RequestError;
    }
 }
