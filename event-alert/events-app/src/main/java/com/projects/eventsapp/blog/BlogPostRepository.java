045f8a224c204d62e42acdc02a0dc2ee01da14b0
Camilo Galeano <galeajua@students.zhaw.ch>
2023-05-12 15:35:04 +0200

    change result order condition

--- event-alert/events-app/src/main/java/com/projects/eventsapp/blog/BlogPostRepository.java
+++ event-alert/events-app/src/main/java/com/projects/eventsapp/blog/BlogPostRepository.java
@@ -6,24 +6,25 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
 
     // all posts of one area ordered by newest (view for the current blog channel)
-    @Query(value = "SELECT * FROM blog_posts bp WHERE bp.area = :area ORDER BY bp.publication_date ASC",
+    @Query(value = "SELECT * FROM blog_posts bp WHERE bp.area = :area ORDER BY bp.publication_date DESC",
             nativeQuery = true)
     List<BlogPost> findByAreaOrderByPublicationDate(@Param("area") Integer area);
 
     // all posts of one area and specific category ordered by newest
-    @Query(value = "SELECT * FROM blog_posts bp WHERE bp.area = :area AND bp.category = :category ORDER BY bp.publication_date DESC",
-            nativeQuery = true)
-    List<BlogPost> findByAreaAndCategoryOrderByPublicationDateDesc(@Param("area") Integer area,
-                                                                   @Param("category") String category);
-
-    // all posts of one area and specific category ordered by oldest
     @Query(value = "SELECT * FROM blog_posts bp WHERE bp.area = :area AND bp.category = :category ORDER BY bp.publication_date ASC",
             nativeQuery = true)
     List<BlogPost> findByAreaAndCategoryOrderByPublicationDateAsc(@Param("area") Integer area,
+                                                                       @Param("category") String category);
+
+    // all posts of one area and specific category ordered by oldest
+    @Query(value = "SELECT * FROM blog_posts bp WHERE bp.area = :area AND bp.category = :category ORDER BY bp.publication_date DESC",
+            nativeQuery = true)
+    List<BlogPost> findByAreaAndCategoryOrderByPublicationDateDesc(@Param("area") Integer area,
                                                                   @Param("category") String category);
 
     // all posts of one area and specific category ordered by most liked
