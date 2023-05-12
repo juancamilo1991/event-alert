e887fa006b111162aa19d96dd5efdbe24f93833a
Camilo Galeano <galeajua@students.zhaw.ch>
2023-05-12 15:35:40 +0200

    new WritePost component

--- event-alert-frontend/src/components/WritePost.tsx
+++ event-alert-frontend/src/components/WritePost.tsx
@@ -1,30 +1,88 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import classes from './styles/WritePost.module.css';
-import { TextField } from '@mui/material';
-import {Link} from 'react-router-dom';
+import { Button, TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { makeRequest } from '../helpers/api-requests/apiRequests';
+import { POST_BLOG_ENDPOINT } from '../api-client/blogPosts';
+import { WritePostProps } from '../types/types';
+import { useHandleApiResponse } from '../customHooks/useHandleApiResponse';
+import { useLocalStorage } from '../authentication/utils/useLocalState';
 
-interface WritePostProps {
-    isLoggedIn: string;
-}
+
+
+
+
 
 const WritePost = (props: WritePostProps) => {
+
+  const [content, setContent] = useState({ title: '', text: '' });
+  const [jwt, setJwt] = useLocalStorage("jwt", "");
+
+  useEffect(() => {
+    setContent({title: '' , text: ''});
+  }, [props.isDeleteInput])
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${jwt}`
+  }
+
+  let correctCategoryName = '';
+
+  const [handleIncomingPosts] = useHandleApiResponse(props);
+
+  if (props.category === 'Verdächtig') {
+    correctCategoryName = 'SUSPICIOUS_BEHAVIOUR'
+  }
+  if (props.category === 'VIP') {
+    correctCategoryName = 'STARS'
+  }
+  if (props.category === 'Unfall') {
+    correctCategoryName = 'ACCIDENT'
+  }
+
+
+
+  function sendBlogPost(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    makeRequest(POST_BLOG_ENDPOINT, props.area, props.displayError, handleIncomingPosts, 'POST', correctCategoryName, '', content.title, content.text, headers);
+  }
+
+  function onChange(e: ChangeEvent<HTMLInputElement>) {
+    setContent({ ...content, [e.target.id]: e.target.value });
+  }
+
   return (
     <>
-        {props.isLoggedIn !== "" ? 
-        <TextField
-        id="outlined-multiline-static"
-        label="Mein Beitrag"
-        multiline
-        rows={4}
-        defaultValue="Schreibe etwas..."
-        className={classes.container}
-        /> :
+      {props.isLoggedIn !== "" ?
+        <form onSubmit={sendBlogPost}>
+          <TextField
+            id='title'
+            label='Titel'
+            placeholder='Titel'
+            value={content.title}
+            onChange={onChange} />
+          <br /> <br />
+          <TextField
+            id="text"
+            label="Mein Beitrag"
+            multiline
+            rows={4}
+            placeholder="Schreibe etwas..."
+            value={content.text}
+            onChange={onChange}
+            className={classes.container}
+          /> <br /> <br />
+          <Button style={{ marginBottom: '20px' }} type='submit' disabled={content.text === '' || content.title === '' || props.area === '' || props.category === ''} variant='contained'>
+            Jetzt posten
+          </Button>
+        </form> :
         <p>
-            <Link to='/login'>Anmelden</Link> oder <Link to='/register'> registrieren </Link> um Beiträge zu posten!
+          <Link to='/login'>Anmelden</Link> oder <Link to='/register'> registrieren </Link> um Beiträge zu posten!
         </p>
-        }
-    
-        </>
+      }
+
+    </>
   )
 }
 
